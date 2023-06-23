0b3e36d1073ee336cfb76b0147c358a8314825b8
anandbaraik <anandbaraik2014@gmail.com>
2023-06-23 21:53:05 +0530

    feat:sorting

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
   const [tableData, setTableData] = useState(snacks);
   const [initialData, setInitialData] = useState(snacks);
   const [search, setSearch] = useState("");
+  const [sortingOrder, setSortingOrder] = useState({
+    id:true,
+    product_name:false,
+    product_weight:false,
+    price:false,
+    calories: false,
+    ingredients: false,
+  })
   const handleSearch = (e) => {
     const searchValue = e.target.value;
     setSearch(searchValue);
@@ -21,6 +29,30 @@ function App() {
         setTableData(initialData);
     }
   }
+
+  const sortBy = (col) => {
+    let sortedData = [];
+    let orderCopy = {...sortingOrder};
+    for (const key in orderCopy) {
+        orderCopy[key] = false;
+        if(key === col) {
+            orderCopy[key] = !sortingOrder[key];
+        }
+    }
+    setSortingOrder(orderCopy);
+    if(col === 'id') {
+        sortedData = sortingOrder.id ? [...initialData]?.sort((a, b) => a?.id - b?.id) : [...initialData]?.sort((a, b) => b?.id - a?.id) ;
+    } else if (col === 'product_name') {
+        sortedData = sortingOrder.product_name ? [...initialData]?.sort((a, b) => a.product_name.localeCompare(b.product_name)) : [...initialData]?.sort((a, b) => b.product_name.localeCompare(a.product_name));
+    } else if (col === 'product_weight') {
+        sortedData = sortingOrder.product_weight ? [...initialData]?.sort((a, b) => a?.product_weight - b?.product_weight) : [...initialData]?.sort((a, b) => b?.product_weight - a?.product_weight);
+    } else if (col === 'price') {
+        sortedData = sortingOrder.price ? [...initialData]?.sort((a, b) => a?.price - b?.price) : [...initialData]?.sort((a, b) => b?.price - a?.price);
+    } else if (col === 'calories') {
+        sortedData = sortingOrder.calories ? [...initialData]?.sort((a, b) => a?.calories - b?.calories) : [...initialData]?.sort((a, b) => b?.calories - a?.calories);
+    }
+    setTableData(sortedData);
+  }
   return (
     <div className='container'>
       <h2>Snack Table</h2>
@@ -28,12 +60,24 @@ function App() {
       <table>
       <thead>
         <tr>
-          <th>ID</th>
-          <th>Product Name</th>
-          <th>Product Weight</th>
-          <th>Price (INR)</th>
-          <th>Calories</th>
-          <th>Ingredients</th>
+          <th onClick={() => { sortBy('id')}}>
+            ID
+        </th>
+          <th onClick={() => sortBy('product_name')}>
+            Product Name
+        </th>
+          <th onClick={() => sortBy('product_weight')}>
+            Product Weight
+            </th>
+          <th onClick={() => sortBy('price')}>
+            Price (INR)
+            </th>
+          <th onClick={() => sortBy('calories')}>
+            Calories
+            </th>
+          <th onClick={() => sortBy('ingredients')}>
+            Ingredients
+        </th>
         </tr>
         </thead>
         <tbody>
@@ -43,7 +87,7 @@ function App() {
                 <tr key={snack.id}>
                     <td>{snack.id}</td>
                     <td>{snack.product_name}</td>
-                    <td>{snack.product_weight}</td>
+                    <td>{snack.product_weight} {'g'}</td>
                     <td>{snack.price}</td>
                     <td>{snack.calories}</td>
                     <td>{snack.ingredients.join(", ")}</td>
